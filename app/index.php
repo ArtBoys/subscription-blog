a1d54c6c81817cf4a0c9f13af8903e8234b9162e
Paweł Rz. <pawel.rzasa@dec-group.pl>
2024-07-01 11:25:42 +0200

    first step

--- app/index.php
+++ app/index.php
@@ -1,11 +1,33 @@
-<?php
-// Enter your code here, enjoy!
-$array = array("1" => "PHP code tester Sandbox Online",
-    "emoji" => "😀 😃 😄 😁 😆", 5 , 5 => 89009,
-    "Random number" => rand(100,999),
-    "PHP Version" => phpversion()
-);
-
-foreach( $array as $key => $value ){
-    echo $key."\t=>\t".$value."\n";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <input type="checkbox" id="toggle">
+    <label for="toggle" class="show-btn">Show Modal</label>
+    <div class="wrapper">
+        <label for="toggle" class="cancel-btn"><i class="fas fa-times"></i></label>
+        <div class="icon"><i class="far fa-envelope">Show Modal</i></div>
+        <div class="content">
+            <header>Become a Subscriber</header>
+            <p>Subscribe to our and get the lastes updates straight to your inbox</p>
+        </div>
+        <form action="#">
+            <div class="field">
+                <input type="text" placeholder="Email Address" required>
+            </div>
+            <div class="field btn">
+                <input type="submit" value="Subscribe">
+            </div>
+        </form>
+        <div class="text">We do not share your information</div>
+    </div>
+    
+</body>
+</html>
